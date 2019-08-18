bcaff14324b21deac821e2d1589120e936ba7448
Filip Brej <filipbrej@Filip-Brejs-MacBook-Pro.local>
2019-08-18 16:37:16 -0500

    Added responsiveness to info cards

--- src/components/InfoCards.component.js
+++ src/components/InfoCards.component.js
@@ -1,71 +1,84 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDollarSign, faCut } from '@fortawesome/free-solid-svg-icons';
+import {
+    faDollarSign,
+    faCut,
+    faComments
+} from '@fortawesome/free-solid-svg-icons';
 import green from '@material-ui/core/colors/green';
-const styles = {
-    card: {
-        minHeight: '200px',
-        width: '30%',
-        margin: '1rem'
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        margin: theme.spacing(2),
+        padding: theme.spacing(2),
+        textAlign: 'center'
     },
     icon: {
-        color: green[800],
-        marginTop: '0.5rem'
+        color: green[800]
     }
-};
+}));
 
 const InfoCards = () => {
+    const classes = useStyles();
+
     return (
-        <Box display='flex' justifyContent='center'>
-            <Paper style={styles.card} elevation='3'>
-                <Typography align='center'>
-                    <FontAwesomeIcon
-                        icon={faCut}
-                        size='2x'
-                        style={styles.icon}
-                    />
-                </Typography>
-                <Typography variant='h6' align='center'>
-                    A cut above the rest.
-                </Typography>
-                <Typography color='textSecondary' align='center'>
-                    Haircuts starting for as little as $10.
-                </Typography>
-            </Paper>
-            <Paper style={styles.card} elevation='3'>
-                <Typography align='center'>
-                    <FontAwesomeIcon
-                        icon={faDollarSign}
-                        size='2x'
-                        style={styles.icon}
-                    />
-                </Typography>
-                <Typography variant='h6' align='center'>
-                    Your style. Your way.
-                </Typography>
-                <Typography color='textSecondary' align='center'>
-                    Haircuts starting for as little as $10.
-                </Typography>
-            </Paper>
-            <Paper style={styles.card} elevation='3'>
-                <Typography align='center'>
-                    <FontAwesomeIcon
-                        icon={faDollarSign}
-                        size='2x'
-                        style={styles.icon}
-                    />
-                </Typography>
-                <Typography variant='h6' align='center'>
-                    Your style. Your way.
-                </Typography>
-                <Typography color='textSecondary' align='center'>
-                    Haircuts starting for as little as $10.
-                </Typography>
-            </Paper>
-        </Box>
+        <Grid container justify='center'>
+            <Grid item xl={3} lg={4} md={4} sm={8} xs={10}>
+                <Paper elevation='3' className={classes.paper}>
+                    <Typography align='center'>
+                        <FontAwesomeIcon
+                            icon={faCut}
+                            size='2x'
+                            className={classes.icon}
+                        />
+                    </Typography>
+                    <Typography variant='h6' align='center'>
+                        A cut above the rest.
+                    </Typography>
+                    <Typography color='textSecondary' align='center'>
+                        Haircuts starting for as little as $10.
+                    </Typography>
+                </Paper>
+            </Grid>
+            <Grid item xl={3} lg={4} md={4} sm={8} xs={10}>
+                <Paper elevation='3' className={classes.paper}>
+                    <Typography align='center'>
+                        <FontAwesomeIcon
+                            icon={faDollarSign}
+                            size='2x'
+                            className={classes.icon}
+                        />
+                    </Typography>
+                    <Typography variant='h6' align='center'>
+                        Your style. Your way.
+                    </Typography>
+                    <Typography color='textSecondary' align='center'>
+                        Haircuts starting for as little as $10.
+                    </Typography>
+                </Paper>
+            </Grid>
+            <Grid item xl={3} lg={4} md={4} sm={8} xs={10}>
+                <Paper elevation='3' className={classes.paper}>
+                    <Typography align='center'>
+                        <FontAwesomeIcon
+                            icon={faComments}
+                            size='2x'
+                            className={classes.icon}
+                        />
+                    </Typography>
+                    <Typography variant='h6' align='center'>
+                        Your style. Your way.
+                    </Typography>
+                    <Typography color='textSecondary' align='center'>
+                        Haircuts starting for as little as $10.
+                    </Typography>
+                </Paper>
+            </Grid>
+        </Grid>
     );
 };
 
--- src/components/logo.component.js
+++ src/components/logo.component.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import green from '@material-ui/core/colors/green';
 
-// Logo styling
-const styles = {
-    color: '#fff',
-    backgroundColor: green[800],
-    fontFamily: 'Pacifico',
-    textAlign: 'center',
-    fontSize: '4rem',
-    lineHeight: '1.4',
-    margin: '0',
-    padding: '0'
-};
+const useStyles = makeStyles(theme => ({
+    logo: {
+        color: '#fff',
+        backgroundColor: green[800],
+        fontFamily: 'Pacifico',
+        textAlign: 'center',
+        fontSize: '3rem',
+        lineHeight: '1.4',
+        margin: '0',
+        padding: '0'
+    }
+}));
 
 // Logo at top of page
 export default function Logo() {
-    return <h1 style={styles}>VillainCuts</h1>;
+    const classes = useStyles();
+    return <h1 className={classes.logo}>VillainCuts</h1>;
 }
